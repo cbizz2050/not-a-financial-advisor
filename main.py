355db54907329ecb9e4e6014a932f11101ce08f3
Cbizz <cbizz2050@protonmail.com>
2023-02-24 00:11:08 -0500

    updated main

--- main.py
+++ main.py
@@ -1,39 +1,45 @@
-from datetime import datetime, timedelta
-from typing import List
-
-import matplotlib.pyplot as plt
+import argparse
+import logging
+import sys
 
 from market_data_ingestor import MarketDataIngestor
-from pattern_detector import Method1Detector
-from database import Database
-from detection_plotter import DetectionEventPlotter
+from pattern_detector import PatternDetector
+from pattern_storage import PatternStorage
 
 
-def main(interval_minutes: int, symbols: List[str], plot_window_minutes: int) -> None:
-    # Instantiate a database object
-    database = Database()
+def main(interval, log_file):
+    # Set up logging
+    logging.basicConfig(filename=log_file, level=logging.INFO)
 
-    # Instantiate a market data ingestor object for each symbol
-    ingestors = [MarketDataIngestor(database, symbol, interval_minutes) for symbol in symbols]
+    # Instantiate objects
+    ingestor = MarketDataIngestor()
+    detector = PatternDetector()
+    storage = PatternStorage()
 
-    # Instantiate one or more pattern detectors
-    detectors = [Method1Detector(database) for _ in range(3)]
-
-    # Instantiate a detection event plotter
-    plotter = DetectionEventPlotter(database, symbols, plot_window_minutes)
-
-    # Run the main loop
     while True:
-        # Fetch the latest market data for each symbol
-        market_data = [ingestor.fetch_latest_market_data() for ingestor in ingestors]
+        try:
+            # Ingest market data
+            ingestor.ingest()
 
-        # Pass the market data to each pattern detector
-        for detector in detectors:
-            for data in market_data:
-                detector.detect(data)
+            # Detect patterns
+            detected_patterns = detector.detect_patterns()
 
-        # Plot any new detection events
-        plotter.plot_new_events()
+            # Store patterns
+            storage.store_pattern(detected_patterns)
+        except Exception as e:
+            logging.error(f"Error occurred: {e}")
+            sys.exit(1)
 
-        # Sleep for some period of time
-        time.sleep(interval_minutes * 60)
+        # Wait for next interval
+        time.sleep(interval)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Stock pattern detection')
+    parser.add_argument('--interval', type=int, default=60,
+                        help='Interval in seconds for ingesting and detecting market data')
+    parser.add_argument('--log', type=str, default='pattern_detection.log',
+                        help='File path for logging output')
+    args = parser.parse_args()
+
+    main(args.interval, args.log)
